ca28bbf9a35d542a867b849bf027c63ca1312cb4
psol <psol@fasoo.com>
2022-05-25 17:26:48 +0900

    init: spring project init

---
+++ src/main/java/psol/solmthing/SolmthingApplication.java
@@ -0,0 +1,13 @@
+package psol.solmthing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolmthingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolmthingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/psol/solmthing/SolmthingApplicationTests.java
@@ -0,0 +1,13 @@
+package psol.solmthing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolmthingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
